25a282cc9df99176c426dfe5a06a9177b0cd986b
root <root@DESKTOP-JRJCNNG>
2023-08-24 09:00:23 -0600

    Using a plugin to properly deploy to s3 now

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    s3Deploy: {
+      awsProfile: 'default',
+      region: 'us-west-1',
+      bucket: 'www.davisbalgley.com',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
+    }
+  }
 })
