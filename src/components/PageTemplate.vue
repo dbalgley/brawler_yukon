bdfab6fb3f9d4bb7d4396279f763665e772e634c
Davis Balgley <davis.balgley@gmail.com>
2023-08-25 12:59:16 -0600

    Many changes, resume page, videos on portfolio, new Homepage, particles

---
+++ src/components/PageTemplate.vue
@@ -0,0 +1,23 @@
+<!-- src/components/PageTemplate.vue -->
+
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <v-card>
+            <v-card-text>
+                <slot></slot>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    defineProps({
+        title: {
+            type: String,
+            required: true
+        }
+    });
+</script>
